af4b124f3af364ece2e54b3262d741fc7c71e6c9
Shaked Erinberg <shakede@wix.com>
2023-07-30 07:45:42 +0300

    remove blueprint

--- src/_codux/boards-global-setup.ts
+++ src/_codux/boards-global-setup.ts
@@ -1,9 +1,1 @@
 import '../index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@blueprintjs/core/lib/css/blueprint.css';
-import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
-import '@blueprintjs/datetime2/lib/css/blueprint-datetime2.css';
-import '@blueprintjs/icons/lib/css/blueprint-icons.css';
-import '@blueprintjs/select/lib/css/blueprint-select.css';
-import '@blueprintjs/table/lib/css/table.css';
-import '@blueprintjs/popover2/lib/css/blueprint-popover2.css';
